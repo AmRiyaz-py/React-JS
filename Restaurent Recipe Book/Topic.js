d340d8d0e47979d83c1a383edf5527cfb9484663
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2021-04-11 00:49:27 +0530

    Topic.js File
    This file is having the Name of the application

---
+++ Restaurent Recipe Book/Topic.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+export default class Topic extends React.Component {
+    render() {
+        return(
+                // <div className="w-15 p-4">
+                <div className="Topic" >
+                    <h2>
+                    Restaurant Recipe Book
+                    </h2>
+                {/* </div> */}
+                </div>
+                  
+            
+        )
+    }
+}
